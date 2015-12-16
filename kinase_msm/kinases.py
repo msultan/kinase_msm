ff84cf637490ad8ccfdfc7be1364e6a2deb3b022
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2015-12-16 12:08:50 -0800

    setup travis

---
+++ kinase_msm/kinases.py
@@ -0,0 +1,8 @@
+#!/bin/env/python
+
+KINASES = frozenset(["btk", "itk", "her2", "mek2",
+                    "fyn", "fgr", "hck" ,"lck" ,"lyn",
+                    "yes", "blk"])
+
+DRUGS = frozenset(["LIG"])
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import numpy
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='kinase_msm',
+    version="0.1",
+    include_dirs=[numpy.get_include()],
+    zip_safe=False,
+    packages=['kinase_msm', 'tests'],
+    author = "Mohammad M. Sultan",
+    author_email = "msultan at stanford dot edu",
+    description = ("Useful scripts across kinases MSMs"),
+    long_description=read('README.md'),
+
+)
