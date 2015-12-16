6800ce3b07c14614cb7876e82f8585c7739d2e4b
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2015-12-16 13:38:47 -0800

    more futzing

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -1,20 +1,10 @@
-MINICONDA=Miniconda-latest-Linux-x86_64.sh
+MINICONDA=Miniconda3-latest-Linux-x86_64.sh
 MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget http://repo.continuum.io/miniconda/$MINICONDA
-if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
-    echo "Miniconda MD5 mismatch"
-    exit 1
-fi
+if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then  echo "Miniconda MD5 mismatch"; exit 1; fi
 bash $MINICONDA -b
-PIP_ARGS="-U"
-
-export PATH=$HOME/miniconda/bin:$PATH
-
-conda update --yes conda
+export PATH=$HOME/miniconda3/bin:$PATH
 conda config --add channels http://conda.binstar.org/omnia
-conda install --yes conda-build
-
-# install extra dependency for style checking
 conda create --yes -n test python=$python `cat requirements.txt | xargs`
 source activate test
 python setup.py install
