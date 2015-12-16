75c3b4c66d9416968e506c5be8d6e1c79ffb3f05
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2015-12-16 13:44:47 -0800

    more futzing

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -4,6 +4,8 @@ wget http://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then  echo "Miniconda MD5 mismatch"; exit 1; fi
 bash $MINICONDA -b
 export PATH=$HOME/miniconda3/bin:$PATH
+conda config --add channels omnia
+conda update conda
 conda create --yes -n test python=$TRAVIS_PYTHON_VERSION `cat requirements.txt | xargs`
 source activate test
 python setup.py install
