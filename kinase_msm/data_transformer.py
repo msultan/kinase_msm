f56e896d8d8de034e6f7cbf2c81f3c46883c15af
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2016-01-04 14:50:54 -0800

    added tests for create tics_array

--- kinase_msm/data_transformer.py
+++ kinase_msm/data_transformer.py
@@ -1,14 +1,16 @@
 #!/bin/env/python
-__author__ = 'muneeb'
 import numpy as np
 
 
 def create_assignment_matrix(assignments):
     """
     :param assignments: Dictionary of assignments keyed by the trajectory name.
-    :return: key_mapping: Dictionary that maps the array index to assignments file name
-    :return: assignment_matrix: matrix of assignments of shape n_traj, length of max traj.
-    Non-used array positions are set to -1 . Useful for doing fast assignment lookups
+    :return: key_mapping: Dictionary that maps the array index to assignments
+    file name
+    :return: assignment_matrix: matrix of assignments of shape n_traj, length
+     of max traj.
+    Non-used array positions are set to -1 . Useful for doing fast
+    assignment lookups
     """
     n_traj = np.shape(assignments.keys())[0]
     max_length = np.max([np.shape(assignments[i]) for i in assignments.keys()])
@@ -36,8 +38,12 @@ def create_tics_array(assignments, kmeans_mdl, tica_data):
     tics_to_use = kmeans_mdl.cluster_centers_.shape[1]
     tics_array = np.empty((n_traj, max_length, tics_to_use))
     tics_array[:, :, :] = np.NAN
+    key_mapping = {}
+
     for index, trjname in enumerate(assignments.keys()):
         current_traj_length = np.shape(assignments[trjname])[0]
         for tic_index in range(tics_to_use):
-            tics_array[index, :current_traj_length, tic_index] = tica_data[trjname][:, tic_index]
-    return tics_array
+            tics_array[index, :current_traj_length, tic_index] = \
+                tica_data[trjname][:, tic_index]
+        key_mapping[index] = trjname
+    return key_mapping, tics_array
--- tests/test_data_transformer.py
+++ tests/test_data_transformer.py
@@ -1,21 +1,52 @@
 #!/bin/env/python
-from kinase_msm.data_transformer import create_assignment_matrix, create_tics_array
+import numpy as np
+from kinase_msm.data_transformer import create_assignment_matrix, \
+    create_tics_array
 
 
 def test_create_assignment_matrix():
-    test_assignments = {}
-    test_assignments["traj1"] = [0, 1, 2]
-    test_assignments["traj2"] = [2, 1, 2]
-    test_assignments["traj3"] = [2, 1, 0, 3]
+    assignments = {}
+    assignments["traj1"] = [0, 1, 2]
+    assignments["traj2"] = [2, 1, 2]
+    assignments["traj3"] = [2, 1, 0, 3]
 
-    key_mapping, assignment_matrix = create_assignment_matrix(test_assignments)
+    key_mapping, assignment_matrix = create_assignment_matrix(assignments)
 
     assert key_mapping[0] == "traj1"
     assert key_mapping[2] == "traj3"
-    assert assignment_matrix.shape == (3,4)
-    assert (assignment_matrix[1][0:3] == test_assignments["traj2"][:3]).all()
+    assert assignment_matrix.shape == (3, 4)
+    assert (assignment_matrix[1][0:3] == assignments[key_mapping[1]][:3]).all()
 
     return
 
+
+class fake_kmeans():
+    def __init__(self, n_states, n_tics):
+        self.cluster_centers_ = np.zeros((n_states, n_tics))
+
+
 def test_create_tics_array():
+    n_traj = 100
+    max_traj_length = 100
+    n_states = 10
+    n_tics = 5
+
+    test_kmeans_mdl = fake_kmeans(n_states, n_tics)
+    tica_data = {}
+    test_assignments = {}
+
+    for i in range(n_traj):
+        test_assignments[str(i)] = np.random.randint(n_states,
+                                                     size=max_traj_length)
+        tica_data[str(i)] = np.random.normal(size=(max_traj_length,
+                                                   n_tics))
+
+    key_mapping, tics_array = create_tics_array(test_assignments,
+                                                test_kmeans_mdl,
+                                                tica_data)
+
+    assert tics_array.shape == (n_traj, max_traj_length, n_tics)
+    for i in range(10):
+        temp_num = np.random.randint(n_traj)
+        assert (tics_array[temp_num] == tica_data[key_mapping[temp_num]]).all()
     return
