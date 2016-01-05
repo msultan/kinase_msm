b802bef2c0ef4b8dc8b08a70f42dfe4c03258cbd
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2016-01-05 11:42:05 -0800

    more futzing

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -6,9 +6,6 @@ bash $MINICONDA -b
 export PATH=$HOME/miniconda3/bin:$PATH
 conda config --add channels omnia
 conda update conda
-conda install --yes --quiet pip
-which pip
-pip install -r requirements.txt
-#conda create --yes -n test python=$TRAVIS_PYTHON_VERSION `cat requirements.txt | xargs`
-#source activate test
+conda create --yes -n test python=$TRAVIS_PYTHON_VERSION `cat requirements.txt | xargs`
+source activate test
 python setup.py install
