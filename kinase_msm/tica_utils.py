41cf9b41967e62b158914b63de958261518b8223
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2016-01-08 17:19:20 -0800

    fixed tica_utils

--- kinase_msm/tica_utils.py
+++ kinase_msm/tica_utils.py
@@ -1,6 +1,7 @@
 #!/bin/evn python
 from kinase_msm.data_loader import load_yaml_file
 import numpy as np
+import os
 from kinase_msm.mdl_analysis import Project, Protein
 from kinase_msm.data_loader import load_frame
 from kinase_msm.data_transformer import create_assignment_matrix, create_tics_array
