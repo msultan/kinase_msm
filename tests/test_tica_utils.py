acd14fc4785df8659ba06c9729aef56e8f6aacf4
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2016-01-20 17:14:05 -0800

    changed tica utils and added support for sampling values in higher dim

---
+++ tests/test_tica_utils.py
@@ -0,0 +1,62 @@
+#!/bin/evn python
+
+from kinase_msm.tica_utils import *
+import mdtraj as mdt
+from kinase_msm.fit_transform_kinase_series import *
+
+from msmbuilder.featurizer import DihedralFeaturizer
+
+if os.path.isdir("tests"):
+    base_dir = os.path.abspath(os.path.join("./tests/test_data"))
+else:
+    base_dir = os.path.abspath(os.path.join("./test_data"))
+
+
+def _test_tic_sampling(yaml_file, protein_name, tic_list, n_frames, scheme):
+    #test to make sure we are sampling right
+    sample_for_all_proteins(yaml_file, [protein_name],
+                            tic_list, n_frames, scheme=scheme)
+    ser = ProteinSeries(yaml_file)
+    prt = Protein(ser, protein_name)
+
+    for tic_index in [0,1]:
+        traj_path = os.path.join(base_dir,yaml_file["mdl_dir"],
+                                 protein_name,"tic%d.xtc"%tic_index)
+        traj_top = os.path.join(base_dir,yaml_file["mdl_dir"],
+                                protein_name, "prot.pdb")
+        tica_traj = mdt.load(traj_path,top=traj_top)
+        feat = DihedralFeaturizer(types=['phi', 'psi','chi1'])
+
+        f = feat.partial_transform(tica_traj)
+        t_f = prt.tica_mdl.transform([f])
+
+        #check that the tic goes from min to max
+        assert t_f[0][0][tic_index] <= t_f[0][-1][tic_index]
+        all_vals = []
+        for traj_tica_data in prt.tica_data.values():
+            all_vals.extend(traj_tica_data[:,tic_index])
+            #sort it because all three sampling schemes use it
+        all_vals = np.sort(all_vals)
+        #make sure the frames are within limits
+        assert (t_f[0][:, tic_index] >= all_vals[0]).all()
+        assert (t_f[0][:, tic_index] <= all_vals[-1]).all()
+    return True
+
+def _test_sample_region(yaml_file, protein_name, tic_region,
+                      n_frames=5, fname="temp.xtc"):
+    sample_tic_region(yaml_file, protein_name, tic_region,
+                      n_frames, fname)
+    assert os.path.isfile(os.path.join(yaml_file["mdl_dir"],
+                                              protein_name,fname))
+    return True
+
+def test_tica_utils():
+    yaml_file = os.path.join(base_dir,"mdl_dir","project.yaml")
+    yaml_file = load_yaml_file(yaml_file)
+    assert _test_tic_sampling(yaml_file, "kinase_1", [0,1], 5, "linear")
+    a={}
+    a[0]=0.3
+    a[1]=0.4
+    assert _test_sample_region(yaml_file,"kinase_1",a)
+    return True
+
