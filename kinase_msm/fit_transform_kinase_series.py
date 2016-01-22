1159de63c94534e86e1a2de96d7193263064175b
msultan <msultan@stanford.edu>
2016-01-22 10:43:19 -0800

    switched batch to minibatch

--- kinase_msm/fit_transform_kinase_series.py
+++ kinase_msm/fit_transform_kinase_series.py
@@ -6,7 +6,7 @@ from msmbuilder.utils import verboseload, verbosedump
 import glob
 from msmbuilder.msm import BayesianMarkovStateModel, MarkovStateModel
 import os
-from msmbuilder.cluster import KMeans
+from msmbuilder.cluster import MiniBatchKMeans as KMeans
 from msmbuilder.dataset import _keynat as keynat
 from kinase_msm.data_loader import enter_protein_data_dir, enter_protein_mdl_dir, load_yaml_file
 
@@ -70,7 +70,7 @@ def fit_protein_kmeans(yaml_file):
     mdl_params = yaml_file["mdl_params"]
     cluster__n_clusters = mdl_params["cluster__n_clusters"]
 
-    kmeans_mdl = KMeans(cluster__n_clusters)
+    kmeans_mdl = KMeans(cluster__n_clusters,batch_size = 100*cluster__n_clusters)
     data = []
 
     for protein in yaml_file["protein_list"]:
