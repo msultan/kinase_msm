a01a4c52d2c0e4396b15f033df8c17d0fa623dd7
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2016-01-27 11:02:50 -0800

    fcking colon

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -9,5 +9,5 @@ conda update conda
 conda info -a
 conda create --yes -n _test python=$TRAVIS_PYTHON_VERSION `cat requirements.txt | xargs`
 source activate _test
-pip install git+https://github.com:msultan/msmbuilder.git@implement_bootstrap_msm
+pip install git+https://github.com/msultan/msmbuilder.git@implement_bootstrap_msm
 python setup.py install
