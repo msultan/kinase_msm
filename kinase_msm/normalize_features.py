126a40e8766519836cba912fe93dc0d886a48340
msultan <msultan@stanford.edu>
2016-05-23 10:17:40 -0700

    added ability to normalize features

---
+++ kinase_msm/normalize_features.py
@@ -0,0 +1,63 @@
+#!/bin/env python
+import os
+import glob
+import mdtraj as mdt
+from kinase_msm.data_loader import load_yaml_file
+from msmbuilder.dataset import _keynat as keynat
+from msmbuilder.utils import verbosedump, verboseload
+import pandas as pd
+import warnings
+from sklearn import preprocessing
+from .featurize_project import _check_output_folder_exists
+from .data_loader import enter_protein_mdl_dir, enter_protein_data_dir
+
+
+
+def normalize_project_series(yaml_file, output_folder="normalized_features",
+                             stride=40,nrm=None):
+    """
+    routine to take a set of proteins features stored in the feature_dir and
+    normalize them by removing the mean and setting variance to 1 using the standard
+    scaler. The normalizer is dumped into the mdl dir.
+    :param yaml_file: The yaml file to work with.
+    :param output_folder: The name of the output folder to dump normalized features in
+    :param stride: The initial stride in files to fit the normalizer with.
+    This is necessary to prevent memory errors. defaults to every 40th file
+    :param nrm: previously fit normalizer. else it uses the standard scaler from
+    scikitlearn
+    :return:
+    """
+    yaml_file = load_yaml_file(yaml_file)
+    #setup normalizer
+    if nrm is None:
+        nrm = preprocessing.StandardScaler()
+
+    all_data = {}
+    for prt in yaml_file["protein_list"]:
+        with enter_protein_data_dir(yaml_file, prt):
+            print(prt)
+            flist = glob.glob("./%s/*.jl"%(yaml_file["feature_dir"]))[::stride]
+            for f in flist:
+                 all_data[f]=verboseload(f)
+
+    seq=[]
+    for i in all_data.keys():
+       seq.extend(all_data[i])
+
+    #fit it
+    nrm.fit(seq)
+    #dump it into the mdl dir.
+    verbosedump(nrm,"%s/nrm.h5"%yaml_file["mdl_dir"])
+
+    for prt in yaml_file["protein_list"]:
+        _check_output_folder_exists(yaml_file, prt, output_folder)
+
+        with enter_protein_data_dir(yaml_file, prt):
+            output_folder = os.path.abspath(output_folder)
+            flist = glob.glob("./%s/*.jl"%(yaml_file["feature_dir"]))
+            for f in flist:
+                res = verboseload(f)
+                res = nrm.transform(res)
+                verbosedump(res,"%s/%s"%(output_folder,os.path.basename(i)))
+
+    return
---
+++ tests/test_normalize_features.py
@@ -0,0 +1,39 @@
+#!/bin/env/python
+
+from kinase_msm.featurize_project import featurize_project_wrapper
+from kinase_msm.normalize_features import normalize_project_series
+from kinase_msm.data_loader import load_yaml_file
+from test_convert_series import _setup_test, _cleanup_test
+from nose.tools import with_setup
+from msmbuilder.utils import verboseload
+import glob
+import os
+import numpy as np
+import mdtraj as mdt
+
+if os.path.isdir("tests"):
+    base_dir = os.path.abspath(os.path.join("./tests/test_data"))
+else:
+    base_dir = os.path.abspath(os.path.join("./test_data"))
+
+
+
+@with_setup(_setup_test, _cleanup_test)
+def test_normalize_features():
+
+    print(base_dir)
+    yaml_file = load_yaml_file(os.path.join(base_dir,"mdl_dir","project.yaml"))
+    normalize_project_series(yaml_file, stride=1)
+
+    all_data=[]
+
+    for kinase in ["kinase_1","kinase_2"]:
+        flist = glob.glob(os.path.join(base_dir, "%s"%kinase , "normalized_features/*.jl"))
+
+        for i in flist:
+            all_data.extend(verboseload(i))
+
+    assert(np.alltrue(np.isclose(np.mean(all_data, axis=1), 0 , atol=1e-1)))
+    assert(np.alltrue(np.isclose(np.std(all_data, axis=1), 1 , atol=0.2)))
+
+
