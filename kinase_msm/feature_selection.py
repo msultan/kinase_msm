1ffb6a723521e4876d9e71cbd98e09ddecbd5189
msultan <msultan@stanford.edu>
2017-02-25 11:35:52 -0800

    switch prt seq to make sure its portien

--- kinase_msm/feature_selection.py
+++ kinase_msm/feature_selection.py
@@ -61,7 +61,7 @@ def _map_residue_ind_seq_ind(yaml_file, protein, aligned_seq, trj=None):
         trj = load_random_traj(yaml_file, protein)
     mapping = {}
     seq_index = 0
-    prt_seq = trj.top.to_fasta(chain=0)
+    prt_seq = ''.join([i.code for i in trj.top.residues if i.is_protein])
     #test to make sure the alignment sequence matches with the protein sequence.
     #get rid of _ from the alignment to account for additions/deletions.
     assert(prt_seq==''.join([i for i in aligned_seq if i!="-"]))
