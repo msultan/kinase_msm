8cad6b27d1147008c06f71af467c05bfd1513154
msultan <msultan@stanford.edu>
2017-10-09 11:32:12 -0700

    fix travis issues

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -9,6 +9,7 @@ conda config --add channels omnia
 conda info -a
 conda create --yes -n _test python=$TRAVIS_PYTHON_VERSION `cat requirements.txt | xargs`
 source activate _test
+pip install --upgrade numpy
 pip install fastcluster
 pip install git+https://github.com/msmbuilder/msmbuilder.git
 python setup.py install
